eb4ee249a99ac08c0c55d949ebae2bf2f2af64fb
Larry <pudding850806@gmail.com>
2017-02-16 10:11:16 +0800

    Fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function switchCase(cases, defaultValue){
     return function(variable){
         for(let prop in cases){
-            if(prop === variable){
+            if(prop == variable){
                 if(typeof cases[prop] === 'function'){
                     return cases[prop](variable)
                 } else {
--- test.js
+++ test.js
@@ -1,24 +1,31 @@
 const should = require('should')
-const switchCase = require('./index')
+const switchhh = require('./index')
 
 describe('#when the case is a value', () => {
     it(`should return corresponding value when the case exists`, (done) => {
-        let val = switchCase({
+        let switcher = switchhh({
             'a': 10,
             'b': 20,
             'c': 30,
-        }, 0)('a')
-        val.should.equal(10)
+            10: 100,
+        }, 0)
+
+        switcher('a').should.equal(10)
+        switcher(10).should.equal(100)
+
         done()
     })
 
     it(`should return default value when the case doesn't exist`, (done) => {
-        let val = switchCase({
+        let switcher = switchhh({
             'a': 10,
             'b': 20,
             'c': 30,
-        }, 0)('d')
-        val.should.equal(0)
+            10: 100,
+        }, 0)
+
+        switcher('something else').should.equal(0)
+
         done()
     })
 
@@ -26,24 +33,31 @@ describe('#when the case is a value', () => {
 
 describe('#when the case is a function', () => {
     it(`should run corresponding function when the case exists`, (done) => {
-        let val = switchCase({
+        let switcher = switchhh({
             'a': () => 10,
             'b': 20,
             'c': 30,
-        }, 0)('a')
-        val.should.equal(10)
+            10: () => 100
+        }, 0)
+
+        switcher('a').should.equal(10)
+        switcher(10).should.equal(100)
+
         done()
     })
 
     it(`should run default function when the case doesn't exist`, (done) => {
-        let val = switchCase({
-            'a': 10,
+        let switcher = switchhh({
+            'a': () => 10,
             'b': 20,
             'c': 30,
+            10: () => 100
         }, function(){
             return 0
-        })('d')
-        val.should.equal(0)
+        })
+
+        switcher('d').should.equal(0)
+
         done()
     })
 })
